0182126ec84412bbad5a7d8f05512c18da278a6f
pranjalimpwork <pranjal.tiwari@impulsiveweb.com>
2025-03-20 16:05:33 +0530

    fix: build issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ittitude_spa",
+  base: "",
   plugins: [react()],
+  assetsInclude: ['**/*.ttf', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
 })
